7e4bcb603ff2c2775bb506de53bd458f4a881f0f
rebeloper <therebeloper@gmail.com>
2022-06-11 07:37:26 +0300

    Initial Commit

---
+++ SwiftUIMongoDB/SwiftUIMongoDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMongoDBApp.swift
+//  SwiftUIMongoDB
+//
+//  Created by Alex Nagy on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMongoDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
